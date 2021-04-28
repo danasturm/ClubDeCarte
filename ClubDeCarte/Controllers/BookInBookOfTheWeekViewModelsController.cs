c55ba834ea2cd1a7c496411a7f70e0a96056ccd7
danasturm <77317175+danasturm@users.noreply.github.com>
2021-04-28 11:19:31 +0300

    Added new View for BookOfTheWeek

---
+++ ClubDeCarte/Controllers/BookInBookOfTheWeekViewModelsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ClubDeCarte.DAL;
+using ClubDeCarte.Models;
+
+namespace ClubDeCarte.Controllers
+{
+    public class BookInBookOfTheWeekViewModelsController : Controller
+    {
+        private BookClubDBContext db = new BookClubDBContext();
+
+        // GET: BookInBookOfTheWeekViewModels
+        public ActionResult Index()
+        {
+            List<BookInBookOfTheWeekViewModel> bookOfTheWeekPairs = new List<BookInBookOfTheWeekViewModel>();
+            IEnumerable<BookOfTheWeek> bookOfTheWeeks = from bW in db.BookOfTheWeeks select bW;
+            foreach (BookOfTheWeek carteaSaptamanii in bookOfTheWeeks)
+            {
+                BookInBookOfTheWeekViewModel aboutBook = new BookInBookOfTheWeekViewModel();
+                aboutBook.BookOfTheWeekID = carteaSaptamanii.BookOfTheWeekID;
+                aboutBook.BookID = carteaSaptamanii.BookID;
+                aboutBook.Description = carteaSaptamanii.Description;
+                aboutBook.ValidFrom = carteaSaptamanii.ValidFrom;
+                aboutBook.ValidTo = carteaSaptamanii.ValidTo;
+                bookOfTheWeekPairs.Add(aboutBook);
+            }
+
+            IEnumerable<Book> books = from b in db.Books select b;
+            foreach (BookInBookOfTheWeekViewModel entry in bookOfTheWeekPairs)
+            {
+                Book aboutBook = books.FirstOrDefault(b => b.BookID == entry.BookID);
+                if (null != aboutBook)
+                {
+                    entry.Title = aboutBook.Title;
+                    entry.UrlPhotoCover = aboutBook.UrlPhotoCover;
+                }
+            }
+
+
+            return View(bookOfTheWeekPairs.ToList());
+        }
+    }
+}
---
+++ ClubDeCarte/Models/BookInBookOfTheWeekViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClubDeCarte.Models
+{
+    public class BookInBookOfTheWeekViewModel
+    {
+        public int BookInBookOfTheWeekViewModelID { get; set; }
+        public int BookOfTheWeekID { get; set; }
+        public int BookID { get; set; }
+
+        [Required(ErrorMessage = "Mandatory field")]
+        [StringLength(250, ErrorMessage = "String too long (max 250 chars")]
+        public string Title { get; set; }
+
+        [Display(Name = "Photo Cover")]
+        [Required(ErrorMessage = "Mandatory field")]
+        public string UrlPhotoCover { get; set; }
+
+        [Required(ErrorMessage = "Mandatory field")]
+        public string Description { get; set; }
+
+        [Display(Name = "Valid From")]
+        [Required(ErrorMessage = "Mandatory field")]
+        public DateTime ValidFrom { get; set; }
+
+        [Display(Name = "Valid To")]
+        [Required(ErrorMessage = "Mandatory field")]
+        public DateTime ValidTo { get; set; }
+    }
+}
